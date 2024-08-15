d0edc54f7f2bab8a02d476b0c9c3069cb9aee67e
pedrosenise <pedrosenise@gmail.com>
2024-08-15 17:17:31 -0300

    Commit Exercício

---
+++ script.js
@@ -0,0 +1,6 @@
+var nome = prompt('Digite seu nome')
+var sobreNome = prompt('Digite seu sobrenmome')
+var anoNasc = parseInt(prompt('Digite seu ano de nascimento'))
+var ano = 2024
+
+alert(`Olá ${nome} ${sobreNome}! Você tem ${ano - anoNasc} anos de idade!`)

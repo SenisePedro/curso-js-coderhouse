25e52482119dfd1f060ded658f06066aa003b00c
SenisePedro <162530611+SenisePedro@users.noreply.github.com>
2024-08-19 18:51:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var nome = prompt('Digite seu nome')
-var sobreNome = prompt('Digite seu sobrenmome')
-var anoNasc = parseInt(prompt('Digite seu ano de nascimento'))
-var ano = 2024
+let nome = prompt('Digite seu nome')
+let sobreNome = prompt('Digite seu sobrenmome')
+let anoNasc = parseInt(prompt('Digite seu ano de nascimento'))
+let ano = 2024
 
 alert(`Olá ${nome} ${sobreNome}! Você tem ${ano - anoNasc} anos de idade!`)
